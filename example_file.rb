abfdb49b5359b9c5a2913b5961531005a647e073
Daniel Madera <dmadera0@gmail.com>
2021-01-06 15:56:11 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,24 @@
+contacts = {
+  "Jon Snow" => {
+    name: "Jon",
+    email: "jon_snow@thewall.we", 
+    favorite_ice_cream_flavors: ["chocolate", "vanilla", "mint chip"],
+        knows: nil
+  },
+  "Freddy Mercury" => {
+    name: "Freddy",
+    email: "freddy@mercury.com",
+    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  }
+}
+ 
+ 
+contacts.each do |person, data|
+  data.each do |attribute, value|
+    if attribute == :name
+      value.name do |name|
+        puts ""
+      end
+    end
+  end
+end
---
+++ learn.rb
@@ -0,0 +1,13 @@
+birthday_kids = {
+    "Timmy" => 9, 
+    "Sarah" => 6, 
+    "Amanda" => 27
+}
+
+def birthday_kids (birthday_kids) 
+  birthday_kids.collect do |kids_name,age|
+  age
+end
+end
+
+  
